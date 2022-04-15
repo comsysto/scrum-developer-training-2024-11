c8bb893bc6f04ae2ab9f13fee9b4d70aa9393393
Christian Kolbe <c.kolbe@reply.de>
2022-04-15 20:41:59 +0200

    add lib implementation

---
+++ src/lib/parseNumber.js
@@ -0,0 +1,17 @@
+function parseNumber(number) {
+    switch (number) {
+        case 'zero': return 0;
+        case 'one': return 1;
+        case 'two': return 2;
+        case 'three': return 3;
+        case 'four': return 4;
+        case 'five': return 5;
+        case 'six': return 6;
+        case 'seven': return 7;
+        case 'eight': return 8;
+        case 'nine': return 9;
+        case 'ten': return 10;
+    }
+}
+
+export default parseNumber;
